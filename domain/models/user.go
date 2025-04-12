04a58681f6d9ab364f65153cba42d8c900c5a452
rofiqabdul <rofiqabdul04@gmail.com>
2025-04-12 10:21:35 +0700

    [SETUP] - add seeder

--- domain/models/user.go
+++ domain/models/user.go
@@ -10,7 +10,8 @@ type User struct {
 	ID          uint      `gorm:"primaryKey;autoIncrement"`
 	UUID        uuid.UUID `gorm:"type:uuid;not null"`
 	Name        string    `gorm:"type:varchar(100);not null"`
-	Pass        string    `gorm:"type:varchar(255);not null"`
+	Username    string    `gorm:"type:varchar(15);not null"`
+	Password    string    `gorm:"type:varchar(255);not null"`
 	PhoneNumber string    `gorm:"type:varchar(15);not null"`
 	Email       string    `gorm:"type:varchar(100);not null"`
 	RoleID      uint      `gorm:"type:uint;not null"`
