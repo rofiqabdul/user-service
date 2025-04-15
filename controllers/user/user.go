cee5869996268fcd73bffb22f50d2d73d7302826
rofiqabdul <rofiqabdul04@gmail.com>
2025-04-15 13:03:02 +0700

    [SETUP] - controllers user and registry

--- controllers/user/user.go
+++ controllers/user/user.go
@@ -117,3 +117,85 @@ func (u *UserController) Register(ctx *gin.Context) {
 		Gin:  ctx,
 	})
 }
+
+func (u *UserController) Update(ctx *gin.Context) {
+	request := &dto.UpdateRequest{}
+	uuid := ctx.Param("uuid")
+
+	err := ctx.ShouldBindJSON(request)
+	if err != nil {
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(request)
+	if err != nil {
+		errMessage := http.StatusText(http.StatusUnprocessableEntity)
+		errResponse := errWrap.ErrValidationResponse(err)
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code:    http.StatusUnprocessableEntity,
+			Message: &errMessage,
+			Data:    errResponse,
+			Err:     err,
+			Gin:     ctx,
+		})
+		return
+	}
+
+	user, err := u.service.GetUser().Update(ctx, request, uuid)
+	if err != nil {
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	response.HTTPResponse(response.ParamHTTPResp{
+		Code: http.StatusOK,
+		Data: user,
+		Gin:  ctx,
+	})
+}
+
+func (u *UserController) GetUserLogin(ctx *gin.Context) {
+	user, err := u.service.GetUser().GetUserLogin(ctx.Request.Context())
+	if err != nil {
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	response.HTTPResponse(response.ParamHTTPResp{
+		Code: http.StatusOK,
+		Data: user,
+		Gin:  ctx,
+	})
+}
+
+func (u *UserController) GetUserByUUID(ctx *gin.Context) {
+	user, err := u.service.GetUser().GetUserByUUID(ctx.Request.Context(), ctx.Param("uuid"))
+	if err != nil {
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	response.HTTPResponse(response.ParamHTTPResp{
+		Code: http.StatusOK,
+		Data: user,
+		Gin:  ctx,
+	})
+}
