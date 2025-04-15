82b084e928993924668177b1b76906ceca866beb
rofiqabdul <rofiqabdul04@gmail.com>
2025-04-15 11:50:52 +0700

    [SETUP] - controllers, login controller & register controller

---
+++ controllers/user/user.go
@@ -0,0 +1,119 @@
+package controllers
+
+import (
+	"net/http"
+	errWrap "user-service/common/error"
+	"user-service/common/response"
+	"user-service/domain/dto"
+	"user-service/services"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type UserController struct {
+	service services.IServiceRegistry
+}
+
+type IUserController interface {
+	Login(*gin.Context)
+	Register(*gin.Context)
+	Update(*gin.Context)
+	GetUserLogin(*gin.Context)
+	GetUserByUUID(*gin.Context)
+}
+
+func NewUserController(service services.IServiceRegistry) IUserController {
+	return &UserController{service: service}
+}
+
+func (u *UserController) Login(ctx *gin.Context) {
+	request := &dto.LoginRequest{}
+
+	err := ctx.ShouldBindJSON(request)
+	if err != nil {
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(request)
+	if err != nil {
+		errMessage := http.StatusText(http.StatusUnprocessableEntity)
+		errResponse := errWrap.ErrValidationResponse(err)
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code:    http.StatusUnprocessableEntity,
+			Message: &errMessage,
+			Data:    errResponse,
+			Err:     err,
+			Gin:     ctx,
+		})
+		return
+	}
+
+	user, err := u.service.GetUser().Login(ctx, request)
+	if err != nil {
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	response.HTTPResponse(response.ParamHTTPResp{
+		Code:  http.StatusOK,
+		Data:  user.User,
+		Token: &user.Token,
+		Gin:   ctx,
+	})
+}
+
+func (u *UserController) Register(ctx *gin.Context) {
+	request := &dto.RegisterRequest{}
+
+	err := ctx.ShouldBindJSON(request)
+	if err != nil {
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(request)
+	if err != nil {
+		errMessage := http.StatusText(http.StatusUnprocessableEntity)
+		errResponse := errWrap.ErrValidationResponse(err)
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code:    http.StatusUnprocessableEntity,
+			Message: &errMessage,
+			Data:    errResponse,
+			Err:     err,
+			Gin:     ctx,
+		})
+		return
+	}
+
+	user, err := u.service.GetUser().Register(ctx, request)
+	if err != nil {
+		response.HTTPResponse(response.ParamHTTPResp{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	response.HTTPResponse(response.ParamHTTPResp{
+		Code: http.StatusOK,
+		Data: user.User,
+		Gin:  ctx,
+	})
+}
